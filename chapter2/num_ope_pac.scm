152801ae610d0c9ab051b1f7a5b8ed160b5f6a04
xzy <xzy@localhost.localdomain>
2018-10-27 17:22:20 +0800

    deriv

---
+++ chapter2/num_ope_pac.scm
@@ -0,0 +1,21 @@
+(define (add x y) (apply-generic 'add x y))
+(define (sub x y) (apply-generic 'sub x y))
+(define (mul x y) (apply-generic 'mul x y))
+(define (div x y) (apply-generic 'div x y))
+
+(define (install-scheme-number-package)
+	(define (tag x)
+		(attach-tag 'scheme-number x))
+	(put 'add '(scheme-number scheme-number)
+		(lambda (x y) (tag (+ x y))))
+	(put 'sub '(scheme-number scheme-number)
+		(lambda (x y) (tag (- x y))))
+	(put 'mul '(scheme-number scheme-number)
+		(lambda (x y) (tag (* x y))))
+	(put 'div '(scheme-number scheme-number)
+		(lambda (x y) (tag (/ x y))))
+	(put 'make 'scheme-number
+		(lambda (x) (tag x)))
+	'done)
+
+
