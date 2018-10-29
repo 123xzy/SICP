3e0c417c0135a34667d3ebbfa28935d71f1f3aff
xzy <xzy@localhost.localdomain>
2018-10-29 15:30:30 +0800

    set

---
+++ chapter2/set_unorder.scm
@@ -0,0 +1,23 @@
+;set without ordered
+;2018.10.29
+
+(define (element-of-set? x set)
+	(cond ((null? set) false)
+		  ((equal? x (car set)) true)
+		  (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+	(if (element-of-set? x set)
+		set
+		(cons x set)))
+
+;the running time is O(n^2)
+(define (intersection-set set1 set2)
+	(cond ((or (null? set1) (null? set2)) '())
+		  ((element-of-set? (car set1) set2)
+		   (cons (car set1)
+				(intersection-set (cdr set1) set2)))
+		  (else (intersection-set (cdr set1) set2))))
+
+;(define (union-set set1 set2)
+		
